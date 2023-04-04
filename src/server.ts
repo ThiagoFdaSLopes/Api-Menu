8928c3edf7d3a9dcfa712fa39601af311b01c912
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 19:17:39 -0300

    feat: Server

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import App from './app';
+import connectToDatabase from './Models/Connection';
+
+class Server {
+  private readonly PORT: number | string;
+  private app: App;
+
+  constructor() {
+    this.PORT = process.env.PORT || 3001;
+    this.app = new App();
+  }
+
+  public start(): void {
+    connectToDatabase()
+      .then(() => {
+        this.app.express.listen(this.PORT, () => {
+          console.log(`Running server on port: ${this.PORT}`);
+        });
+      })
+      .catch((error) => {
+        console.log('Connection with database generated an error:\r\n');
+        console.error(error);
+        console.log('\r\nServer initialization cancelled');
+        process.exit(0);
+      });
+  }
+}
+
+export default Server;
