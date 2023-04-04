3e600e7b4482f41be3f410722c94917fc0aa0525
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 19:54:41 -0300

    feat: Util GenerateToken

---
+++ src/utils/Token/GenerateToken.ts
@@ -0,0 +1,14 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
+import fs from 'fs';
+import { IUserLogin } from '../../Api/Interfaces/IUserLogin';
+
+export const createToken = (body: IUserLogin) => {
+  const configToken: SignOptions = {
+    algorithm: 'HS256',
+    expiresIn: '1d',
+  };
+  const secret = fs.readFileSync(`${__dirname}/jwt.key`, { encoding: 'utf-8' });
+  return jwt.sign(body, secret, configToken);
+};
+
+export default createToken;
