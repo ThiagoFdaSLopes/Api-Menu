94bb5bc4afce8f7e5e9a7ef05fff1169e2114e6b
Thiago <programadorthiagolopes@gmail.com>
2023-04-05 11:31:27 -0300

    feat: Route userLogin

--- src/Api/Routes/UserRoutes.ts
+++ src/Api/Routes/UserRoutes.ts
@@ -1,1 +1,14 @@
 import { Request, Response, Router, NextFunction } from 'express';
+import UserController from '../Controllers/UserController';
+
+const userController = new UserController();
+
+const userRouter = Router();
+
+userRouter.post('/auth/login', (
+  req: Request, 
+  res: Response, 
+  next: NextFunction,
+) => userController.UserLogin(req, res, next));
+
+export default userRouter;
