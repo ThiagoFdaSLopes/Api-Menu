2f00f9a493bc53a4e3fc25f89dfe074d92c99e68
Thiago <programadorthiagolopes@gmail.com>
2023-04-05 11:31:02 -0300

    fix: Routes add no app

--- src/Api/app.ts
+++ src/Api/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandle';
+import userRouter from './Routes/UserRoutes';
 
 class App {
   public express: express.Application;
@@ -7,18 +8,18 @@ class App {
   constructor() {
     this.express = express();
     this.middleware();
-    // this.routes();
+    this.routes();
     this.express.get('/', (_req, res) => res.status(200).json({ message: 'Api Running' }));
   }
 
   private middleware(): void {
     this.express.use(express.json());
+  }
+  
+  private routes(): void {
+    this.express.use('/', userRouter);
     this.express.use(ErrorHandler.handle);
   }
-
-  // private routes(): void {
-  //   this.express.use('/auth/login', UserRouter);
-  // }
 }
 
 export default App;
