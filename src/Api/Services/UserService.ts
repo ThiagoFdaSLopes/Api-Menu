94e048394a997d2df0f5e7bd1e8d55e45ace7c46
Thiago <programadorthiagolopes@gmail.com>
2023-04-05 11:31:40 -0300

    feat: Service Login

--- src/Api/Services/UserService.ts
+++ src/Api/Services/UserService.ts
@@ -1,6 +1,8 @@
-// import IUserLogin from '../Interfaces/IUserLogin';
+import md5 from 'md5';
+import IUserLogin from '../Interfaces/IUserLogin';
 import UserODM from '../../Database/Models/UserODM';
-// import ClassError from '../../utils/Error/ClassError';
+import ClassError from '../../utils/Error/ClassError';
+import { createToken } from '../../utils/Token/GenerateToken';
 
 class UserService {
   private _modelODM: UserODM;
@@ -9,9 +11,19 @@ class UserService {
     this._modelODM = modelODM;
   }
 
-  // public async login(user: IUserLogin): Promise<string> {
-    
-  // }
+  public async login(user: IUserLogin): Promise<string> {
+    try {
+      const pass = md5(user.password);
+      const UserLogin = { email: user.email, password: pass };
+      const result = await this._modelODM.findByOne(UserLogin);
+      if (!result) throw new ClassError('Email ou senha incorretos', 404);
+      const { email, password, id } = result;
+      const token = createToken({ email, password, id });
+      return token;
+    } catch (error) {
+      throw new ClassError(`${(error as Error).message}`, 500);
+    }
+  }
 }
 
 export default UserService;
