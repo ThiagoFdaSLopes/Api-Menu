53183dbd5e4d2d11111d856a9e81c925f91a9589
Thiago <programadorthiagolopes@gmail.com>
2023-04-06 12:37:26 -0300

    feat: Authentication Token nas rotas

--- src/Api/Routes/CategoryRoutes.ts
+++ src/Api/Routes/CategoryRoutes.ts
@@ -1,14 +1,19 @@
 import { Request, Response, Router, NextFunction } from 'express';
 import CategoryController from '../Controllers/CategoryController';
+import authMiddleware from '../Middlewares/AuthToken';
 
 const categoryController = new CategoryController();
 
 const categoryRouter = Router();
 
-categoryRouter.get('/', (
-  req: Request, 
-  res: Response, 
-  next: NextFunction,
-) => categoryController.GetAllCategorys(req, res, next));
+categoryRouter.get(
+  '/',
+  authMiddleware, 
+  (
+    req: Request, 
+    res: Response, 
+    next: NextFunction,
+  ) => categoryController.GetAllCategorys(req, res, next),
+);
 
 export default categoryRouter;
