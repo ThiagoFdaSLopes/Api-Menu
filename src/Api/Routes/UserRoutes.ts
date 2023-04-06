8c953524ad2ff6d1b8002e77feb2564c6c2f6cf9
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-04-06 19:23:34 -0300

    fix: Add Middleware BodyLogin

--- src/Api/Routes/UserRoutes.ts
+++ src/Api/Routes/UserRoutes.ts
@@ -1,14 +1,19 @@
 import { Request, Response, Router, NextFunction } from 'express';
 import UserController from '../Controllers/UserController';
+import ValidateBodyLogin from '../Middlewares/ValidateBodyLogin';
 
 const userController = new UserController();
 
 const userRouter = Router();
 
-userRouter.post('/auth/login', (
-  req: Request, 
-  res: Response, 
-  next: NextFunction,
-) => userController.UserLogin(req, res, next));
+userRouter.post(
+  '/auth/login', 
+  ValidateBodyLogin,
+  (
+    req: Request, 
+    res: Response, 
+    next: NextFunction,
+  ) => userController.UserLogin(req, res, next),
+);
 
 export default userRouter;
